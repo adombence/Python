57521b4684ae135c29cd324e3db62bdadf0f6c99
adombence <adom.bence97@gmail.com>
2021-01-22 22:07:24 +0100

    Create main.py

---
+++ image_converter/main.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import glob
+
+for count, filename in enumerate(glob.glob('input/*.*'), start=1):
+    im = Image.open(filename)
+    rgb_im = im.convert('RGB')
+    rgb_im.save(('output/img' + str(count) + '.webp'), quality=75)
+    print(count, filename)
