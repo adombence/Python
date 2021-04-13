98abd4d0439b0c6ca62344e2f4eb6207a358c8d3
Bence Ádom <44981129+adombence@users.noreply.github.com>
2021-04-13 09:26:15 +0200

    image converter

--- image_converter/main.py
+++ image_converter/main.py
@@ -3,6 +3,12 @@ import glob
 
 for count, filename in enumerate(glob.glob('input/*.*'), start=1):
     im = Image.open(filename)
+    #opacity
+    #im.putalpha(128)
+    #rgba
     rgb_im = im.convert('RGBA')
+    #átméretezés
+    #width, height = rgb_im.size
+    #rgb_im = rgb_im.resize((width//6, height//6))
     rgb_im.save(('output/img' + str(count) + '.webp'), quality=75)
     print(count, filename)
